83b54ad941c3eacced853cd8ff34d77bb3d93513
GHDBASHEN <ghdbashen@gmail.com>
2025-12-03 17:35:57 +0530

    Initial

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PORT'] = 3306
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = '1234'
 app.config['MYSQL_DB'] = 'quizapp'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
